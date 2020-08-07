d69eb6b5dc055ed2330d6906ec1e77679dfff2a3
dayang4321 <dayang4321@users.noreply.github.com>
2020-08-07 09:04:02 +0100

    final

--- FE_PAGES/assets/js/custom-js/new-advertisers-payment.js
+++ FE_PAGES/assets/js/custom-js/new-advertisers-payment.js
@@ -19,5 +19,29 @@
         });
     });
 
+$('.transaction-table .transaction-status').addClass(function (index) {
+    console.log($('.transaction-table .transaction-status').contents()[index].data);
+    if ($('.transaction-table .transaction-status').contents()[index].data === "Successful") { return "success-text" }
+    else return "warning-text"
+})
+
+    $('#transaction-modal').on('show.bs.modal', function (event) {
+        //var button = $(event.relatedTarget) // Button that triggered the modal
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        const modalData = (index, param) => { return $(button).closest("tr").children().children('.card-title, .info-text').contents()[index][param] }
+       // console.log( $(button).closest("tr").children().children('.card-title,.info-text').html())
+        $('.modal-data').children().html((index) => {
+            if (modalData(index, "data")) { return modalData(index, "data") }
+            else { return modalData(index, "outerHTML") }
+        });
+
+        $('.modal .transaction-status').addClass(function (index) {
+            console.log($('.modal .transaction-status').contents()[index].data);
+            if ($('.modal .transaction-status').contents()[index].data === "Successful") { return "success-text" }
+            else return "warning-text"
+        });
+      })
 
 
